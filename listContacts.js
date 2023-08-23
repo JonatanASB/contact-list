64b66ef8140d170d1efe635095df314a44d4a996
Jonatan03 <goldenblack394@gmail.com>
2023-08-23 14:03:21 -0500

    SDe realizo la funcion de actualizar un contacto

--- listContacts.js
+++ listContacts.js
@@ -23,12 +23,12 @@ const listaContactos = [
 ];
 
 function añadirContacto() {
-    let id = prompt("Ingresa tu Id: ");
-    let nombre = prompt("Ingresa tu nombre: ");
+    let id = prompt("Ingresa el Id: ");
+    let nombre = prompt("Ingresa los nombre: ");
     let apellido = prompt("Ingresa los apellidos: ");
-    let telefono = prompt("Ingresa tu telefono: ");
-    let ciudad = prompt("Ingresa tu ciudad: ");
-    let direccion = prompt("Ingresa tu direccion: ");
+    let telefono = prompt("Ingresa el telefono: ");
+    let ciudad = prompt("Ingresa la ciudad: ");
+    let direccion = prompt("Ingresa la direccion: ");
 
     const lista = {
         id: id,
@@ -43,16 +43,48 @@ function añadirContacto() {
 }
 
 function mostrarLista() {
-    for (let lista of  listaContactos) {
+    for (let i = 0; i < listaContactos.length; i++) {
         console.log("--------------------");
-        console.log(lista, listaContactos[lista]);
+        console.log("Indice " + [i]);
+        console.log( listaContactos[i]);
     }
 }
 
 const eliminarContacto = () => {
-    let id = prompt("Ingresa el id a eliminar: "); 
+    let id = prompt("Ingresa el id a eliminar: ");
     let eliminarContacto = id;
-     let index = listaContactos.findIndex(itr => itr.id == eliminarContacto);
+    let index = listaContactos.findIndex(itr => itr.id == eliminarContacto);
     listaContactos.splice(index, 1);
     console.log("Contacto eliminado");
+}
+
+const actualizarContacto = () => {
+    // escogemos la posicion en el objeto  
+    let indice = prompt("Ingresa el indice a actualizar: ");
+    alert("Ingrese uno a uno los nueos valores");
+
+    //esta condicion es para buscar el indice selecionado y asi actualizarlo
+    if (indice >= 0 && indice < listaContactos.length) {
+        let actContacto = listaContactos[indice];
+        let id = prompt("Ingresa el nuevo id: ");
+        let nomb = prompt("Ingresa tel nuevo nombre: ");
+        let apell = prompt("Ingresa el nuevo apellido: ");
+        let tel = prompt("Ingresa el nuevo telefono: ");
+        let ciu = prompt("Ingresa la nueva ciudad: ");
+        let dir = prompt("Ingresa la nueva direccion: ");
+
+        actContacto.id = id,
+        actContacto.nombre = nomb,
+        actContacto.apellido = apell,
+        actContacto.telefono = tel,
+        actContacto.ciudad = ciu,
+        actContacto.direccion = dir
+    } else {
+        console.log("Índice fuera de rango.");
+    }
+
+
+
+
+
 }
